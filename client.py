740d7af3cc34c9848225e42bc957903f722f0908
BernardoGiordano <bernardogiordano369@gmail.com>
2019-03-09 09:46:46 +0100

    Fix client

--- client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-with open('test.pk7', 'rb') as f:
-    data = f.read()
-
-res = requests.post(url='http://118.200.51.43:5000/legalize', data=data, headers={'Content-Type': 'application/octet-stream'})
-
-with open('out.pk7', 'wb') as f:
-    f.write(res.content)
--- server.py
+++ server.py
@@ -1,21 +1,42 @@
 import subprocess
 import os
 import io
+import uuid
+from datetime import datetime
 from flask import Flask, url_for, request, jsonify, send_file
+
 app = Flask(__name__)
 
-@app.route('/legalize', methods = ['POST'])
+DIR_OUTPUT = "output"
+
+def ensureFolderExists(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+def datePath():
+    return datetime.now().strftime("%Y%m%d")
+
+@app.route('/api/legalize', methods = ['POST'])
 def api_legalize():
-    if request.headers['Content-Type'] == 'application/octet-stream':
-        with open('input.pkx', 'wb') as f:
-            f.write(request.data)
-        proc = subprocess.Popen(['CoreConsole.exe', '-alm', '--version', 'us', '-i', 'input.pkx'], stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        fname = os.listdir(os.getcwd() + '/output')[0]
-        with open(os.getcwd() + '/output/' + fname, 'rb') as f:
-            pkx = f.read()
-        os.remove(os.getcwd() + '/output/' + fname)
-        return send_file(io.BytesIO(pkx), attachment_filename=fname)
+    try:
+        if request.headers['Content-Type'] == 'application/octet-stream':
+            if not request.data or not request.headers.get('Version'):
+                print("shit")
+                return None
+            storeFolder = os.path.join(DIR_OUTPUT, datePath())
+            ensureFolderExists(storeFolder)
+            inputPath = os.path.join(storeFolder, str(uuid.uuid4()) + ".pkx")
+            with open(inputPath, 'wb') as f:
+                f.write(request.data)
+            proc = subprocess.Popen(['CoreConsole/bin/Debug/CoreConsole.exe', '-alm', '--version', request.headers.get('Version'), '-i', inputPath], stdout=subprocess.PIPE, shell=True)
+            (out, err) = proc.communicate()
+            with open(inputPath, 'rb') as f:
+                pkx = f.read()
+            return send_file(io.BytesIO(pkx), attachment_filename="output.pkx")
+    except Exception as e:
+        print(e)
+        return None
 
 if __name__ == '__main__':
+    ensureFolderExists(DIR_OUTPUT)
     app.run(host='0.0.0.0', port=5000)
